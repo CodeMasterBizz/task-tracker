cd195c9ee84d83964e07c34c3f53f89f7da22cd8
Abhishek Shanbhag <abhishek@bizzlearn.co>
2025-04-18 14:36:20 -0400

    Add GitHub Pages deployment workflow

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 const supabaseUrl = 'https://vddfyefjmhsjwuluigax.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkZGZ5ZWZqbWhzand1bHVpZ2F4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4MTI1MjYsImV4cCI6MjA2MDM4ODUyNn0.RD-sC9R7JOhklYQ7M8DSesJmcRuOXqfveVH8C4vCHDo'
